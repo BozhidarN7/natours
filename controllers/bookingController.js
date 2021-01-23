355a92b0a811ccd5a5d93ccfdb07eaad75706fd2
Bozhidar Nemski <bojonemski@gmail.com>
2021-01-23 15:21:12 +0200

    Implemented stripe messages

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -6,26 +6,33 @@ const catchAsync = require('../utils/catchAsync');
 const factory = require('./handlerFactory');
 
 exports.getCheckoutSession = catchAsync(async(req, res, next) => {
-    // 1. Get the currently booked tour
+    // 1) Get the currently booked tour
     const tour = await Tour.findById(req.params.tourId);
-    // 2. Create checkout session
+    // console.log(tour);
+
+    // 2) Create checkout session
     const session = await stripe.checkout.sessions.create({
         payment_method_types: ['card'],
-        //success_url: `${req.protocol}://${req.get('host')}/?tour=${req.params.tourId}&user=${req.user.id}&price=${tour.price}`,
-        success_url: `${req.protocol}://${req.get('host')}/my-tours`,
+        // success_url: `${req.protocol}://${req.get('host')}/my-tours/?tour=${
+        //   req.params.tourId
+        // }&user=${req.user.id}&price=${tour.price}`,
+        success_url: `${req.protocol}://${req.get('host')}/my-tours?alert=booking`,
         cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
         customer_email: req.user.email,
         client_reference_id: req.params.tourId,
         line_items: [{
             name: `${tour.name} Tour`,
             description: tour.summary,
-            images: [`${req.protocol}://${req.get('host')}/img/tours/${tour.imageCover}`],
+            images: [
+                `${req.protocol}://${req.get('host')}/img/tours/${tour.imageCover}`
+            ],
             amount: tour.price * 100,
             currency: 'usd',
             quantity: 1
         }]
     });
-    // 3. Create session as response
+
+    // 3) Create session as response
     res.status(200).json({
         status: 'success',
         session
@@ -35,35 +42,29 @@ exports.getCheckoutSession = catchAsync(async(req, res, next) => {
 const createBookingCheckout = async session => {
     const tour = session.client_reference_id;
     const user = (await User.findOne({ email: session.customer_email })).id;
-    const price = session.line_items[0].amount / 100;
+    const price = session.display_items[0].amount / 100;
     await Booking.create({ tour, user, price });
-}
+};
 
 exports.webhookCheckout = (req, res, next) => {
     const signature = req.headers['stripe-signature'];
+
     let event;
     try {
-        event = stripe.webhooks.constructEvent(req.body, signature, process.env.STRIPE_WEBHOOK_SECRET);
+        event = stripe.webhooks.constructEvent(
+            req.body,
+            signature,
+            process.env.STRIPE_WEBHOOK_SECRET
+        );
     } catch (err) {
         return res.status(400).send(`Webhook error: ${err.message}`);
     }
 
-    if (event.type === 'checkout.session.completed') {
+    if (event.type === 'checkout.session.completed')
         createBookingCheckout(event.data.object);
-    }
 
     res.status(200).json({ received: true });
-
 };
-// exports.createBookingCheckout = catchAsync(async(req, res, next) => {
-//     // This is only TEMPRORARY because it's not secure: everyone can make bookings without paying
-//     const { tour, user, price } = req.query;
-//     if (!tour && !user && !price) {
-//         return next();
-//     }
-
-//     await Booking.create({ tour, user, price });
-// });
 
 exports.createBooking = factory.createOne(Booking);
 exports.getBooking = factory.getOne(Booking);
--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const viewController = require('../controllers/viewController');
 const authController = require('../controllers/authController');
-const bookingController = require('../controllers/bookingController');
 
 const router = express.Router();
+
+router.use(viewController.alerts);
 
 router.get('/', authController.isLoggedIn, viewController.getOverview);
 router.get('/tour/:slug', authController.isLoggedIn, viewController.getTour);
