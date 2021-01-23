ceb9a305034bc3c1af2c0e39815fea21140304a5
Bozhidar Nemski <bojonemski@gmail.com>
2021-01-23 13:31:21 +0200

    Improved stripe implementation

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoute');
 const viewRouter = require('./routes/viewRoutes');
 const bookingRouter = require('./routes/bookingRoutes')
+const bookingController = require('./controllers/bookingController');
 const { validate } = require('./models/userModel');
 
 // Start express app
@@ -58,6 +59,9 @@ const limiter = rateLimit({
     message: 'Too many requests from this IP, please try again in an hour!'
 });
 app.use('/api', limiter);
+
+app.post('/webhook-checkout', express.raw({ type: 'application/json' }), bookingController.webhookCheckout);
+
 
 // Body parser, reading data from the body into req.body
 app.use(express.json({ limit: '10kb' }));
