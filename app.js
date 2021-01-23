338e691d384159424adc43e7a295a5bbe6f163bc
Bozhidar Nemski <bojonemski@gmail.com>
2021-01-23 15:09:29 +0200

    Improved stripe implementation 3

--- app.js
+++ app.js
@@ -60,7 +60,8 @@ const limiter = rateLimit({
 });
 app.use('/api', limiter);
 
-app.post('/webhook-checkout', express.raw({ type: 'application/json' }), bookingController.webhookCheckout);
+// Stripe webhook, BEFORE body-parser, because stripe needs the body as stream
+app.post('/webhook-checkout', express.raw({ type: 'application:json' }), bookingController.webhookCheckout);
 
 
 // Body parser, reading data from the body into req.body
--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -4,7 +4,6 @@ const User = require('../models/userModel');
 const Booking = require('../models/bookingModel');
 const catchAsync = require('../utils/catchAsync');
 const factory = require('./handlerFactory');
-const AppError = require('../utils/appError');
 
 exports.getCheckoutSession = catchAsync(async(req, res, next) => {
     // 1. Get the currently booked tour
