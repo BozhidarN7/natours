bed3cfb3aecd7c35156533075485f55bb58848ec
Bozhidar Nemski <bojonemski@gmail.com>
2021-01-23 11:27:02 +0200

    Implemented CORS

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
+const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -28,6 +29,16 @@ app.set('views', path.join(__dirname, 'views'));
 
 
 // 1. GLOBAL MIDDLEWARES
+// Implement CORS
+app.use(cors());
+// Access-Control-Allow_origin * 
+// api.natours.com, natours.com
+// app.use(cors({
+//     origin: 'http://www.natours.com'
+// }));
+
+app.options('*', cors());
+
 // Serving static files
 // app.use(express.static(`${__dirname}/public/`));
 app.use(express.static(path.join(__dirname, 'public')));
